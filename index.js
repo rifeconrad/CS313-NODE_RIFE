a5e7199d8dd5ba7e6e5a81e35d7a29ddfb58fdb0
rifeconrad <rif16001@byui.edu>
2019-06-21 05:35:49 -0400

    commit

--- index.js
+++ index.js
@@ -3,18 +3,10 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 
-express().use(express.static(path.join(__dirname, 'public')));
-
-express().set('views', path.join(__dirname, 'views'));
-
-express().set('view engine', 'ejs');
-
-express().get('/', function (req, res) {
-   res.render('pages/index');
-});
-
-express().get('/getRate', function (req, res) {
-   res.render('pages/rate');
-});
-
-express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/getRate', (req, res) => res.render('ponder/pages/rate'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
